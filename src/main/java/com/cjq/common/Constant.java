86d1521ec156fa514a741f280261cdab3bf543d5
Julian Chi <759178651@qq.com>
2024-04-28 01:15:49 +0800

    v-7.12.1.2. Add EsJdbcConfig and update README.md

---
+++ src/main/java/com/cjq/common/Constant.java
@@ -0,0 +1,8 @@
+package com.cjq.common;
+
+public class Constant {
+    public static final String _INDEX = "_index";
+    public static final String _TYPE = "_type";
+    public static final String _ID = "_id";
+    public static final String _VERSION = "_version";
+}
