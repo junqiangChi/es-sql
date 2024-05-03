6c2239de47bb050bbb1067b4ecd05e779efd4eb3
Julian Chi <759178651@qq.com>
2024-05-04 03:43:34 +0800

    v-7.12.1.5 add test

--- src/test/java/com/cjq/JdbcTest.java
+++ src/test/java/com/cjq/JdbcTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import java.sql.*;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Properties;
 
@@ -24,21 +25,89 @@ public class JdbcTest {
             properties.put(EsJdbcConfig.ES_URL, "jdbc:elasticsearch://node001:9200");
             properties.put(EsJdbcConfig.USERNAME, "elastic");
             properties.put(EsJdbcConfig.PASSWORD, "123456");
+            properties.put(EsJdbcConfig.INCLUDE_INDEX, "true");
             DruidDataSource dds = (DruidDataSource) ElasticSearchDruidDataSourceFactory.createDataSource(properties);
 
             DruidPooledConnection connection = dds.getConnection();
-            String sql = "SELECT DATA_ID, COMMON_TYPE from ori_data_im where DATA_ID = '1' limit 10";
+            /*
+                PUT my_index_1/
+                {
+                  "mappings": {
+                    "properties": {
+                      "f1": {
+                        "type": "long"
+                      },
+                      "f2": {
+                        "type": "text",
+                        "analyzer": "ik_smart"
+                      },
+                      "f3": {
+                        "type": "keyword"
+                      },
+                      "f4": {
+                        "type": "text"
+                      }
+                    }
+                  }
+                }
+
+               PUT my_index_1/_doc/1
+                {
+                  "f1": 1,
+                  "f2": "你好，世界",
+                  "f3": "cjq945",
+                  "f4": "cjq"
+                }
+
+                PUT my_index_1/_doc/2
+                {
+                  "f1": 2,
+                  "f2": "你好，世界",
+                  "f3": "cjq945",
+                  "f4": "cjq"
+                }
+                PUT my_index_1/_doc/3
+                {
+                  "f1": 3,
+                  "f2": "你好，世界",
+                  "f3": "cjq945",
+                  "f4": "cjq"
+                }
+                PUT my_index_1/_doc/4
+                {
+                  "f1":4,
+                  "f2": "你好，世界"
+                }
+
+                PUT my_index_1/_doc/5
+                {
+                  "f1": 5,
+                  "f2": "你好，世界"
+                }
+             */
+            String sql = "SELECT * FROM my_index_1 t1 WHERE f1 between 2 and 3";
+            //  String sql = "SELECT * FROM my_index_1 t1 WHERE f1 not between 1 and 3";
+            //  String sql = "SELECT * FROM my_index_1 t1 WHERE f1 in (1,2,3)";
+            //  String sql = "SELECT * FROM my_index_1 t1 WHERE f1 not in (1,2,3)";
+            //  String sql = "SELECT * FROM my_index_1 t1 WHERE f1 > 3";
+            //  String sql = "SELECT * FROM my_index_1 WHERE f2 match '你好'";
+            //  String sql = "SELECT * FROM my_index_1 WHERE f2 term '你好'";
+            //  String sql = "SELECT * FROM my_index_1 WHERE f3 not like 'cjq%'";
+            //  String sql = "SELECT * FROM my_index_1 WHERE f4 = 'cjq'";
+            /**
+             * 以上测试全部正常
+             */
             PreparedStatement ps = connection.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             ResultSetMetaData metaData = rs.getMetaData();
             int columnCount = metaData.getColumnCount();
-            Map<String, String> columnMap = new HashMap<>();
+            Map<String, String> columnMap = new LinkedHashMap<>();
             for (int i = 1; i <= columnCount; i++) {
                 columnMap.put(metaData.getColumnName(i), metaData.getColumnTypeName(i));
             }
             int count = 0;
             while (rs.next()) {
-                HashMap<String, Object> resultMap = new HashMap<>();
+                HashMap<String, Object> resultMap = new LinkedHashMap<>();
                 columnMap.forEach((key, value) -> {
                     try {
                         resultMap.put(key, rs.getObject(key));
